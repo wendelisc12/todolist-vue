8ae8c9397f1b531d716a01811109b2c01b9a3cea
Wendel <wendel.isaac12@gmail.com>
2024-07-04 14:06:52 -0300

    feat: adicionando bootstrap e criando layout

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,40 @@
 </script>
 
 <template>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>Você possui 7 tarefas pendentes</p>
+    </header>
+    <form action="">
+      <div class="row">
+        <div class="col">
+          <input type="text" placeholder="Digite a descrição da tarefa" class="form-control">
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select class="form-control">
+            <option value="todas">Todas tarefas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+
+    <ul class="list-group mt-4">
+      <li class="list-group-item">
+        <input type="checkbox" >
+        <label for="" class="ms-3">
+          Estudar ES6
+        </label>
+      </li>
+    </ul>
+  </div>
+
+</template>
  
 <style scoped>
 
